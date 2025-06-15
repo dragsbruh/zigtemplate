45309954d199c865006cd83c6dbd06e7323ec387
dragsbruh <dragsbruh@protonmail.com>
2025-06-15 12:51:01 +0530

    better formatting

--- {{cookiecutter.project_slug}}/build.zig
+++ {{cookiecutter.project_slug}}/build.zig
@@ -16,21 +16,16 @@ pub fn build(b: *std.Build) void {
     });
 
     b.installArtifact(exe);
-    const run_cmd = b.addRunArtifact(exe);
 
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
 
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    if (b.args) |args| run_cmd.addArgs(args);
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
-
+    const exe_unit_tests = b.addTest(.{ .root_module = exe_mod });
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}.zig
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}.zig
@@ -1,6 +1,6 @@
-pub fn start(allocator: std.mem.Allocator) anyerror!void {
-    std.debug.print("hello from {s}!\n", .{"{{cookiecutter.project_slug}}"});
-    allocator.free(try allocator.alloc(u8, 32));
-}
-
 const std = @import("std");
+
+pub fn start(allocator: std.mem.Allocator) anyerror!void {
+    _ = allocator;
+    std.debug.print("hello from {s}!\n", .{"{{cookiecutter.project_slug}}"});
+}
