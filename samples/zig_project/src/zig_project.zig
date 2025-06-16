563f46f2fef4f85c95b3da399693f9dc4e3001d8
dragsbruh <dragsbruh@protonmail.com>
2025-06-16 18:49:56 +0530

    add sample project

---
+++ samples/zig_project/src/zig_project.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn start(allocator: std.mem.Allocator) anyerror!void {
+    const input = "123 67 89,99";
+
+    var list = std.ArrayList(u32).init(allocator);
+    // Ensure the list is freed at scope exit.
+    // Try commenting out this line!
+    defer list.deinit();
+
+    var it = std.mem.tokenizeAny(u8, input, " ,");
+    while (it.next()) |num| {
+        const n = try std.fmt.parseInt(u32, num, 10);
+        try list.append(n);
+    }
+
+    std.debug.print("{any}\n", .{list.items});
+}
